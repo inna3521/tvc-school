033f0ae72a56d1c4b6aa73129632b4f04cd0c729
klequis <klequis@gmail.com>
2017-10-31 10:33:07 -0700

    moved all styles to index.css

--- app/src/components/Home/ReactTOC/RestaurantAppTOC/RA07MenuEdit/index.jsx
+++ app/src/components/Home/ReactTOC/RestaurantAppTOC/RA07MenuEdit/index.jsx
@@ -12,56 +12,42 @@ import P from 'elements/P'
 import A from 'elements/A'
 import InlineTag from 'elements/InlineTag'
 import Hint from 'elements/Hint'
-import iMenu from './img/menu.png'
+import iMenuEdit from './img/menu-edit.png'
 
 const RA07MenuEdit = (props) => {
   return (
     <div>
       <PageTitle title='Making the Menu' back={props.match} />
       <Section title='Introduction' level={1}>
-        <P>In this section you will create the restaurant's Menu. For now it will read only. Editing is part of a later challenge. Here is a picture of the Menu:</P>
-        <Img src={iMenu} medium alt='menu read only' />
+        <P>In this section you will create a component named MenuEdith which will allow the user to edit the menu. Below is the UI for this component.</P>
+        <Img src={iMenuEdit} medium alt='menu read only' />
       </Section>
       <Section title='Requirements' level={1}>
         The Menu must meet the these requirements:
         <UL>
-          <LI>Will have a element <InlineTag angle>h2</InlineTag> element with the text 'Our Menu'</LI>
-          <LI>Will display all menu items from /src/menu-data.js</LI>
-          <LI>Each menu item will have a title, the description and price.</LI>
-          <LI>Each menu item will be on its own row.</LI>
-          <LI>Create two components Menu &amp; MenuItem</LI>
+          <LI>The user will be able to change the title, description and price.</LI>
+          <LI>Changes to the data will be stored in state</LI>
         </UL>
       </Section>
       <Section title='Tips' level={1}>
         <Hint>
-          <p>When you import menu-data.js it will be an array.</p>
-        </Hint>
-        <Hint>
-          <p>Use <A href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map'>Arrray.Map()</A> to loop through the data and render the individual menu items.</p>
-        </Hint>
-        <Hint>
-          <p>Create a MenuItem component to render an individual menu item.</p>
+          <p>Use the onChange() event</p>
         </Hint>
       </Section>
 
       <Section title='Structure & Styling' level={1}>
         <P>The button below reveals part of the answer. Don't click it until you have met the requiremets outlined above.</P>
-        <Hint title='JSX for Menu'>
+        <Hint title='JSX for MenuEdit'>
           <Pre
             code={[
-              "<div className='menu'>",
-              "  <h2>Our Menu</h2>",
-              "  {renderItems}",
-              "</div>",
+
             ]}
           />
         </Hint>
-        <Hint title='CSS for Menu'>
+        <Hint title='CSS for MenuEdit'>
           <Pre
             code={[
-              ".menu h2 {",
-              "  text-align: center;",
-              "}",
+              
             ]}
           />
         </Hint>
