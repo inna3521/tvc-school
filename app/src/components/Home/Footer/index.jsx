9c553005521685e059c37a4be955c22d0a2e06e4
klequis <klequis@gmail.com>
2017-11-12 17:27:00 -0800

    added icons to footer

--- app/src/components/Home/Footer/index.jsx
+++ app/src/components/Home/Footer/index.jsx
@@ -1,46 +1,60 @@
 // Footer
 import React from 'react';
 import styles from './style.css';
-// import classNames from 'classnames'
-// import Img from './../../elements/Img'
-// import Resources from './Resources'
+import classNames from 'classnames'
+import A from '../../elements/A'
 
 const Footer = () => {
-  const gitHubStyle = {
-    color: 'black',
-    marginRight: 25,
-    marginLeft: 25,
-  }
-  const meetupStyle = {
-    color: '#F64060',
-    marginRight: 25,
-    marginLeft: 25,
-  }
-  const facebookStyle = {
-    color: '#3B5998',
-    marginRight: 25,
-    marginLeft: 25,
-  }
+  const gitHubStyle = classNames({
+    'fa': true,
+    'fa-github': true,
+    'fa-2x': true,
+    [styles.github]: true,
+    [styles.faIcon]: true,
+  })
+  const meetupStyle = classNames({
+    'fa': true,
+    'fa-meetup': true,
+    'fa-2x': true,
+    'fa-icon': true,
+    [styles.faIcon]: true,
+    [styles.meetup]: true,
+  })
+  const facebookStyle = classNames({
+    'fa': true,
+    'fa-facebook': true,
+    'fa-2x': true,
+    'fa-icon': true,
+    [styles.faIcon]: true,
+    [styles.facebook]: true,
+  })
+  const slackStyle = classNames({
+    'fa': true,
+    'fa-slack': true,
+    'fa-2x': true,
+    'fa-icon': true,
+    [styles.faIcon]: true,
+    [styles.slack]: true,
+  })
   return (
     <footer>
-      {/* <Resources /> */}
       <div className={styles.footerContainer}>
         <div className={styles.faBtns}>
-          {/* <a href="https://github.com/trivalleycoders-org" style={gitHubStyle}> */}
-          {/* <i className="fa fa-github fa-2x" aria-hidden="true"></i> */}
-          {/* </a> */}
-          <a href="https://www.meetup.com/trivalleycoders//" style={meetupStyle}>
-            <i className="fa fa-meetup fa-2x meetup-logo" aria-hidden="true"></i>
-          </a>
-          <a href="https://www.facebook.com/groups/free.code.camp.sanramon/" style={facebookStyle}>
-            <i className="fa fa-facebook-official fa-2x" aria-hidden="true"></i>
-          </a>
+          <A href="https://github.com/trivalleycoders-org">
+            <i className={gitHubStyle} aria-hidden="true"></i>
+          </A>
+          <A href="https://www.meetup.com/trivalleycoders//">
+            <i className={meetupStyle} aria-hidden="true"></i>
+          </A>
+          <A href="http://tvc-slack-signup.s3-website-us-west-2.amazonaws.com">
+            <i className={slackStyle} aria-hidden="true"></i>
+          </A>
+          <A href="https://www.facebook.com/groups/free.code.camp.sanramon/">
+            <i className={facebookStyle} aria-hidden="true"></i>
+          </A>
+
         </div>
-        <p>&copy; 2017 TriValley Coders&trade; All rights reserved<br/>Don't code alone!&trade;</p>
-        {/* <div className={styles.mdnLicense}>
-          <div>Some content derived from MDN by <a href="https://developer.mozilla.org/en-US/docs/Learn/HTML/Introduction_to_HTML/Getting_started"><span className={styles.mdnContributors}></span>Mozilla Contributors</a></div>
-          <div>Licensed under <a href="http://creativecommons.org/licenses/by-sa/2.5/">CC-BY-SA 2.5</a></div>
-        </div> */}
+        <p className={styles.copy}>&copy; 2017 TriValley Coders&trade; All rights reserved<br/>Don't code alone!&trade;</p>
       </div>
     </footer>
   )
