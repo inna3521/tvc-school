e79836a5b977bfe6f8a758d98774efc32b4ece78
klequis <klequis@gmail.com>
2017-11-22 22:55:35 -0800

    added TOC button and relevant styles

--- app/src/components/Home/PageTitle/index.jsx
+++ app/src/components/Home/PageTitle/index.jsx
@@ -7,40 +7,54 @@ import NavButton from './NavButton'
 import PropTypes from 'prop-types'
 
 const PageTitle = ({ routes, location}) => {
+  // Location
   const { pathname } = location
   const route = routes.filter((r, index) => (r.path === pathname))[0]
   const { index, title } = route
-
-  console.log('index', index)
-  const prevIndex = index - 1
-  console.log('prevIndex', prevIndex)
-  const nextIndex = index + 1
-  console.log('nextIndex', nextIndex)
   const maxIndex = routes.length - 1
-  console.log('maxIndex', maxIndex)
-  const previous = index > 0
-  const next = index < maxIndex
-  const previousRoute = previous
-    ? routes[index - 1].path
-    : ''
-  const nextRoute = next
-    ? routes[index].path
-    : ''
-  return (
-    <div className={styles.pageHeader}>
-      {previous
-        ? <NavButton className={styles.back} path={previous}>Back</NavButton>
-        : <div className={styles.spacer}></div>
-      }
+  const previous = index > 0 // t/f value
+  const next = index < maxIndex // t/f value
+  // TOC
+  console.log('location', location)
+  let previousTOCPath = ''
+  for (let i=index-1; i>=0; i--) {
+    if (routes[i].type === 'toc') {
+      previousTOCPath = routes[i].path
+      break
+    }
+  }
+  console.log('previousTOCPath', previousTOCPath)
+  // Buttons
+  const previousButton = previous
+    ? <NavButton className={styles.back} path={routes[index - 1].path}><i className="fa fa-chevron-left" aria-hidden="true"></i> {routes[index - 1].title}</NavButton>
+    : <div className={styles.spacer}></div>
+  const nextButton = next
+    ? <NavButton className={styles.back} path={routes[index + 1].path}>{routes[index + 1].title} <i className="fa fa-chevron-right" aria-hidden="true"></i></NavButton>
+    : <div className={styles.spacer}></div>
+  const previousTOCButton = previousTOCPath
+    ? <NavButton className={styles.back} path={previousTOCPath}>TOC <i className="fa fa-chevron-up" aria-hidden="true"></i></NavButton>
+    : null
 
-      <div className={styles.titles}>
-        <h1 className={styles.title}>{title}</h1>
-        {/* <h2 className={styles.subTitle}>{subTitle}</h2> */}
+  return (
+
+    <div className={styles.pageHeader}>
+      <div className={styles.navBar}>
+        <div className={styles.left}>
+          {previousButton}
+        </div>
+        <div className={styles.center}>
+          {previousTOCButton}
+        </div>
+        <div className={styles.right}>
+          {nextButton}
+        </div>
       </div>
-      {next
-        ? <NavButton className={styles.back} path={next}>Next</NavButton>
-        : <div className={styles.spacer}></div>
-      }
+      <div className={styles.pageTitles}>
+        <div className={styles.titles}>
+          <h1 className={styles.title}>{title}</h1>
+          {/* <h2 className={styles.subTitle}>{subTitle}</h2> */}
+        </div>
+      </div>
     </div>
   )
 }
