c6a5675b475762c2b116c0ea4e04b69b39d78b63
klequis <klequis@gmail.com>
2018-01-31 18:34:05 -0800

    making outlines

--- app/src/components/Home/present/C1/index.jsx
+++ app/src/components/Home/present/C1/index.jsx
@@ -5,7 +5,7 @@ import Section from 'elements/Section'
 const C1 = () => {
   return (<div id='C1'>
     <Section>
-      <iframe className="frame" src="https://repl.it/@klequis/c1?lite=true" scrolling="no" frameBorder="no" allowTransparency="true" allowFullScreen="true" sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals" title="c1" width="800" height="800"></iframe>
+      <iframe className="frame" src="htt[ps://repl.it/@klequis/c1?lite=true" scrolling="no" frameBorder="no" allowTransparency="true" allowFullScreen="true" sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals" title="c1" width="800" height="800"></iframe>
     </Section>
     <Section>
       <iframe title="x" width="100%" height="800px" frameBorder="0" scrolling="no" allowTransparency="true" src="https://tech.io/playground-widget/7b6745f2c57179db1e850f3a9792994332051/welcome/60559/Luke%2C%20how%20many%20stars%20are%20there%20in%20these%20galaxies%3F"></iframe>
